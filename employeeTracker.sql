1bd8b763f066deaf70ed452199910fc9d2ccfe3b
Kevin Holder <kholder777@yahoo.com>
2020-07-16 22:23:03 -0500

    Resolved all issues. Time to make a video showing functionality.

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -11,15 +11,16 @@ CREATE TABLE department(
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL (8,2) NOT NULL,
+  salary DECIMAL(8,2) NOT NULL,
   department_id INT NOT NULL,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR (30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
+  manager_id VARCHAR(30) NOT NULL,
   FOREIGN KEY (role_id) REFERENCES role(id)
 );
