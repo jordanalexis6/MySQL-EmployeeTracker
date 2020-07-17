917532e459d05d397108e31332c11216cd606e4d
Jordan <jordan.a.neill@gmail.com>
2020-07-17 11:54:59 -0500

    added a seed.sql file

--- employeeTracker.js
+++ employeeTracker.js
@@ -4,272 +4,272 @@ const cTable = require("console.table");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-	host: "localhost",
+  host: "localhost",
 
-	// Your port; if not 3306
-	PORT: process.env.PORT || 3306,
+  // Your port; if not 3306
+  PORT: process.env.PORT || 3306,
 
-	// Your username
-	user: "root",
+  // Your username
+  user: "root",
 
-	// Your password
-	password: "password",
-	database: "employee_DB",
+  // Your password
+  password: "password",
+  database: "employee_DB",
 });
 
 // connect to the mysql server and sql database
 connection.connect(function (err) {
-	if (err) throw err;
-	init();
+  if (err) throw err;
+  init();
 });
 
 const questions = [
-	{
-		type: "list",
-		name: "actionChoice",
-		message: "What would you like to do?",
-		choices: ["ADD", "VIEW", "UPDATE"],
-	},
+  {
+    type: "list",
+    name: "actionChoice",
+    message: "What would you like to do?",
+    choices: ["ADD", "VIEW", "UPDATE"],
+  },
 ];
 const questions2 = [
-	{
-		type: "list",
-		name: "targetChoice",
-		message: "What would you like to target?",
-		choices: ["employee", "role", "department"],
-	},
+  {
+    type: "list",
+    name: "targetChoice",
+    message: "What would you like to target?",
+    choices: ["employee", "role", "department"],
+  },
 ];
 
 const employeeQuestions = [
-	{
-		name: "first_name",
-		type: "input",
-		message: "What is your first name?",
-	},
-	{
-		name: "last_name",
-		type: "input",
-		message: "What is your last name?",
-	},
-	{
-		name: "role_id",
-		type: "input",
-		message: "What is your role id?",
-	},
-	{
-		name: "manager_id",
-		type: "input",
-		message: "What is your manager id?",
-	},
+  {
+    name: "first_name",
+    type: "input",
+    message: "What is your first name?",
+  },
+  {
+    name: "last_name",
+    type: "input",
+    message: "What is your last name?",
+  },
+  {
+    name: "role_id",
+    type: "input",
+    message: "What is your role id?",
+  },
+  {
+    name: "manager_id",
+    type: "input",
+    message: "What is your manager id?",
+  },
 ];
 const roleQuestions = [
-	{
-		name: "title",
-		type: "input",
-		message: "What is your role title?",
-	},
-	{
-		name: "salary",
-		type: "input",
-		message: "What is your current salary",
-	},
-	{
-		name: "department_id",
-		type: "input",
-		message: "What is your department ID?",
-	},
+  {
+    name: "title",
+    type: "input",
+    message: "What is your role title?",
+  },
+  {
+    name: "salary",
+    type: "input",
+    message: "What is your current salary",
+  },
+  {
+    name: "department_id",
+    type: "input",
+    message: "What is your department ID?",
+  },
 ];
 const departmentQuestions = [
-	{
-		name: "name",
-		type: "input",
-		message: "What is your department name?",
-	},
+  {
+    name: "name",
+    type: "input",
+    message: "What is your department name?",
+  },
 ];
 //not MVP.
 // const allQuestions = [employeeQuestions, roleQuestions, departmentQuestions];
 
 // if response is addChoice run through function to add based on what they choose
 function init() {
-	inquirer.prompt(questions).then((response) => {
-		if (response.actionChoice === "UPDATE") {
-			//display current employee table
-			console.table(response2.results);
-			role();
-		} else {
-			inquirer.prompt(questions2).then((response2) => {
-				switch (response.actionChoice) {
-					case "ADD":
-						if (response2.targetChoice === "employee") {
-							employee();
-						}
-						if (response2.targetChoice === "role") {
-							role();
-						}
-						if (response2.targetChoice === "department") {
-							department();
-						}
-						break;
-					case "VIEW":
-						if (response2.targetChoice === "employee") {
-							// employee();
-							view(response2.targetChoice);
-						}
-						if (response2.targetChoice === "role") {
-							view(response2.targetChoice);
-						}
-						if (response2.targetChoice === "department") {
-							view(response2.targetChoice);
-						}
-						break;
-					case "EXIT":
-						connection.end();
-						break;
-					// code block
-				}
-			});
-		}
-	});
+  inquirer.prompt(questions).then((response) => {
+    if (response.actionChoice === "UPDATE") {
+      //display current employee table
+      console.table(response2.results);
+      role();
+    } else {
+      inquirer.prompt(questions2).then((response2) => {
+        switch (response.actionChoice) {
+          case "ADD":
+            if (response2.targetChoice === "employee") {
+              employee();
+            }
+            if (response2.targetChoice === "role") {
+              role();
+            }
+            if (response2.targetChoice === "department") {
+              department();
+            }
+            break;
+          case "VIEW":
+            if (response2.targetChoice === "employee") {
+              // employee();
+              view(response2.targetChoice);
+            }
+            if (response2.targetChoice === "role") {
+              view(response2.targetChoice);
+            }
+            if (response2.targetChoice === "department") {
+              view(response2.targetChoice);
+            }
+            break;
+          case "EXIT":
+            connection.end();
+            break;
+          // code block
+        }
+      });
+    }
+  });
 }
 // addChoice functions questions based on what they want to add
 function employee() {
-	//check if dept exists
-	//check if role exists
-	inquirer.prompt(employeeQuestions).then(function (answers) {
-		// when finished prompting, insert a new item into the db with that info
-		console.log(answers);
-		connection.query(
-			"INSERT INTO employee SET ?",
-			{
-				first_name: answers.first_name,
-				last_name: answers.last_name,
-				role_id: answers.role_id,
-				manager_id: answers.manager_id,
-			},
-			function (err) {
-				if (err) throw err;
-				// console.table();
-			}
-		);
-	});
+  //check if dept exists
+  //check if role exists
+  inquirer.prompt(employeeQuestions).then(function (answers) {
+    // when finished prompting, insert a new item into the db with that info
+    console.log(answers);
+    connection.query(
+      "INSERT INTO employee SET ?",
+      {
+        first_name: answers.first_name,
+        last_name: answers.last_name,
+        role_id: answers.role_id,
+        manager_id: answers.manager_id,
+      },
+      function (err) {
+        if (err) throw err;
+        // console.table();
+      }
+    );
+  });
 }
 function role() {
-	inquirer.prompt(roleQuestions).then(function (answers) {
-		// when finished prompting, insert a new item into the db with that info
+  inquirer.prompt(roleQuestions).then(function (answers) {
+    // when finished prompting, insert a new item into the db with that info
 
-		connection.query(
-			"INSERT INTO role SET ?",
-			{
-				title: answers.title,
-				salary: answers.salary,
-				department_name: answers.department_name,
-			},
-			function (err) {
-				if (err) throw err;
-				// console.table();
-			}
-		);
-	});
+    connection.query(
+      "INSERT INTO role SET ?",
+      {
+        title: answers.title,
+        salary: answers.salary,
+        department_name: answers.department_name,
+      },
+      function (err) {
+        if (err) throw err;
+        // console.table();
+      }
+    );
+  });
 }
 function department() {
-	inquirer.prompt(departmentQuestions).then(function (answers) {
-		// when finished prompting, insert a new item into the db with that info
-		connection.query(
-			"INSERT INTO department SET ?",
-			{
-				name: answers.name,
-			},
-			function (err) {
-				if (err) throw err;
-				// console.table();
-			}
-		);
-	});
+  inquirer.prompt(departmentQuestions).then(function (answers) {
+    // when finished prompting, insert a new item into the db with that info
+    connection.query(
+      "INSERT INTO department SET ?",
+      {
+        name: answers.name,
+      },
+      function (err) {
+        if (err) throw err;
+        // console.table();
+      }
+    );
+  });
 }
 
 // if response is viewChoice
 function view(response2) {
-	// console.log(response);
-	// inquirer.prompt(viewQuestions).then((response3) => {
-	// console.log(response3);
-	switch (response2) {
-		case "employee":
-			connection.query("SELECT * FROM employee limit 50", (err, results) => {
-				if (err) throw err;
+  // console.log(response);
+  // inquirer.prompt(viewQuestions).then((response3) => {
+  // console.log(response3);
+  switch (response2) {
+    case "employee":
+      connection.query("SELECT * FROM employee limit 50", (err, results) => {
+        if (err) throw err;
 
-				console.log("Data received from Db:");
-				console.table(results);
-			});
-			break;
-		case "role":
-			connection.query("SELECT * FROM role limit 50", (err, results) => {
-				if (err) throw err;
+        console.log("Data received from Db:");
+        console.table(results);
+      });
+      break;
+    case "role":
+      connection.query("SELECT * FROM role limit 50", (err, results) => {
+        if (err) throw err;
 
-				console.log("Data received from Db:");
-				console.table(results);
-			});
-			break;
-		case "department":
-			connection.query("SELECT * FROM department limit 50", (err, results) => {
-				if (err) throw err;
+        console.log("Data received from Db:");
+        console.table(results);
+      });
+      break;
+    case "department":
+      connection.query("SELECT * FROM department limit 50", (err, results) => {
+        if (err) throw err;
 
-				console.log("Data received from Db:");
-				console.table(results);
-			});
-			break;
+        console.log("Data received from Db:");
+        console.table(results);
+      });
+      break;
 
-		default:
-		// code block
-	}
-	// });
+    default:
+    // code block
+  }
+  // });
 }
 // init();
 // if response is updateChoice
 function update() {
-	inquirer.prompt(questions).then((response) => {
-		console.log(response);
-		inquirer.prompt(updateQuestions).then((response4) => {
-			console.log(response4);
-			switch (response4.updateChoice) {
-				case "employee":
-					connection.query(
-						//----------------this is where the current problem is happening: Where id = ?    -------------------------
-						"UPDATE employee SET first_name = ?, last_name = ? where id = ?",
-						["Lil", "Leipzig", 3],
-						(err, result) => {
-							if (err) throw err;
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+    inquirer.prompt(updateQuestions).then((response4) => {
+      console.log(response4);
+      switch (response4.updateChoice) {
+        case "employee":
+          connection.query(
+            //----------------this is where the current problem is happening: Where id = ?    -------------------------
+            "UPDATE employee SET first_name = ?, last_name = ? where id = ?",
+            ["Lil", "Leipzig", 3],
+            (err, result) => {
+              if (err) throw err;
 
-							console.log(`Changed ${result.changedRows} row(s)`);
-						}
-					);
-					break;
-				case "role":
-					connection.query(
-						"UPDATE role SET title = ?, salary = ? where id = ?",
-						["Bobby", "Leipzig", 3],
-						(err, result) => {
-							if (err) throw err;
+              console.log(`Changed ${result.changedRows} row(s)`);
+            }
+          );
+          break;
+        case "role":
+          connection.query(
+            "UPDATE role SET title = ?, salary = ? where id = ?",
+            ["Bobby", "Leipzig", 3],
+            (err, result) => {
+              if (err) throw err;
 
-							console.log(`Changed ${result.changedRows} row(s)`);
-						}
-					);
-					break;
-				case "department":
-					connection.query(
-						"UPDATE department SET name = ? Where id = ?",
-						["Leipzig", 3],
-						(err, result) => {
-							if (err) throw err;
+              console.log(`Changed ${result.changedRows} row(s)`);
+            }
+          );
+          break;
+        case "department":
+          connection.query(
+            "UPDATE department SET name = ? Where id = ?",
+            ["lil", "Leipzig", 3],
+            (err, result) => {
+              if (err) throw err;
 
-							console.log(`Changed ${result.changedRows} row(s)`);
-						}
-					);
-					break;
-				default:
-				// code block
-			}
-		});
-	});
+              console.log(`Changed ${result.changedRows} row(s)`);
+            }
+          );
+          break;
+        default:
+        // code block
+      }
+    });
+  });
 }
 // res.send(result.toString());
 
